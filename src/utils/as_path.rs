9a4a6daa0620b9a24deffc5c76e6ba32efed0e40
Alberto Corona <alberto@0x1a.us>
2015-06-23 23:51:37 -0500

    as_path,pathmod: add tests, documentation
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/utils/as_path.rs
+++ src/utils/as_path.rs
@@ -4,13 +4,30 @@
 
 use std::path::{PathBuf,Path};
 
-/// A trait for treating String and str as Path and PathBuf
+/// A trait for treating String and str as `Path` and `PathBuf`
 pub trait AsPath {
     /// Returns a borrowed `Path`
     ///
+    /// # Example
+    /// ```
+    /// use rpf::AsPath;
+    /// use std::path::Path;
+    ///
+    /// let path = "/etc/test/string".as_path().clone();
+    /// assert_eq!(path, Path::new("/etc/test/string"));
+    /// ```
     fn as_path(&self) -> &Path;
+
     /// Returns a `PathBuf`
     ///
+    /// # Example
+    /// ```
+    /// use rpf::AsPath;
+    /// use std::path::PathBuf;
+    ///
+    /// let pathbuf = "/var/log/test".as_pathbuf();
+    /// assert_eq!(pathbuf, PathBuf::from("/var/log/test"));
+    /// ```
     fn as_pathbuf(&self) -> PathBuf;
 }
 
@@ -22,4 +39,38 @@ impl AsPath for String {
     fn as_pathbuf(&self) -> PathBuf {
         PathBuf::from(self)
     }
+}
+
+impl AsPath for str {
+    fn as_path(&self) -> &Path {
+        Path::new(self)
+    }
+
+    fn as_pathbuf(&self) -> PathBuf {
+        PathBuf::from(self)
+    }
+}
+
+#[test]
+fn test_as_path_string() {
+    assert_eq!("/etc/test/dir".to_string().as_path().clone(),
+        Path::new("/etc/test/dir"));
+}
+
+#[test]
+fn test_as_pathbuf_string() {
+    assert_eq!("/etc/test/dir".to_string().as_pathbuf(),
+        PathBuf::from("/etc/test/dir"));
+}
+
+#[test]
+fn test_as_path_str() {
+    assert_eq!("/etc/test/dir".as_path().clone(),
+        Path::new("/etc/test/dir"));
+}
+
+#[test]
+fn test_as_pathbuf_str() {
+    assert_eq!("/etc/test/dir".as_pathbuf(),
+        PathBuf::from("/etc/test/dir"));
 }
--- src/utils/pathmod.rs
+++ src/utils/pathmod.rs
@@ -5,8 +5,8 @@
 use std::path::{PathBuf,Path};
 use std::fs::{PathExt};
 
-/// Adds functions for retrieving first and last components as well as getting
-/// a relative path
+/// Adds some useful functions for manipulating and retrieving information from
+/// paths
 pub trait PathMod {
     /// Returns a `PathBuf` of `&self`'s last component
     ///
@@ -24,7 +24,6 @@ pub trait PathMod {
     /// Returns a `PathBuf` of `&self`'s first component
     ///
     /// # Example
-    ///
     /// ```
     /// use rpf::PathMod;
     /// use std::path::PathBuf;
@@ -38,7 +37,6 @@ pub trait PathMod {
     /// Returns a `PathBuf` of `&self` relative from `rel_from`
     ///
     /// # Example
-    ///
     /// ```
     /// use rpf::PathMod;
     /// use std::path::PathBuf;
@@ -49,11 +47,10 @@ pub trait PathMod {
     /// ```
     fn rel_to<T: AsRef<Path>>(&self, rel_from: &T) -> PathBuf;
 
-    /// Returns a string for an path, returns a blank string if not able to
+    /// Returns a `&str` for a path, returns a blank string if unable to
     /// get a string for the path
     ///
     /// # Example
-    ///
     /// ```
     /// use rpf::PathMod;
     /// use std::path::PathBuf;
@@ -64,8 +61,30 @@ pub trait PathMod {
     /// ```
     fn as_str(&self) -> &str;
 
+    /// Returns a `String` for a path, returns an empty string if unable to get
+    /// a string for a path
+    ///
+    /// # Example
+    /// ```
+    /// use rpf::PathMod;
+    /// use std::path::PathBuf;
+    ///
+    /// let path_string = PathBuf::from("/var/log/test").as_string();
+    /// assert_eq!(path_string, "/var/log/test".to_string());
+    /// ```
     fn as_string(&self) -> String;
 
+    /// Returns true if a given path is a symbolic link, returns false if the
+    /// path is a regular file, a directory, or does not exist
+    ///
+    /// # Example
+    /// ```
+    /// use rpf::PathMod;
+    /// use std::path::PathBuf;
+    ///
+    /// let path = PathBuf::from("test-symlink");
+    /// assert_eq!(path.is_symlink(), false);
+    /// ```
     fn is_symlink(&self) -> bool;
 }
 
@@ -100,10 +119,12 @@ impl PathMod for PathBuf {
     }
 
     fn is_symlink(&self) -> bool {
-        if self.is_dir() || self.is_file() {
-            return false
-        }
-        return true
+        if self.exists() {
+            if self.is_dir() || self.is_file() {
+                return false
+            }
+            return true
+        } else { return false }
     }
 }
 
@@ -138,10 +159,12 @@ impl PathMod for Path {
     }
 
     fn is_symlink(&self) -> bool {
-        if self.is_dir() || self.is_file() {
-            return false
-        }
-        return true
+        if self.exists() {
+            if self.is_dir() || self.is_file() {
+                return false
+            }
+            return true
+        } else { return false }
     }
 }
 
@@ -167,4 +190,10 @@ fn test_pathmod_as_str() {
 fn test_pathmod_as_string() {
     let string = Path::new("/var/log/test").as_string();
     assert_eq!(string, "/var/log/test".to_string());
+}
+
+#[test]
+fn test_pathmod_is_symlink() {
+    let path = Path::new("/var/log/test");
+    assert_eq!(path.is_symlink(), false);
 }
