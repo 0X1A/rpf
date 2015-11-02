d35f67ea50340387665dc2e2f440afd088c83895
Alberto Corona <ac@albertocorona.com>
2015-11-02 13:39:13 -0600

    travis: test auto doc upload
    Signed-off-by: Alberto Corona <ac@albertocorona.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright (C) 2015, Alberto Corona <alberto@0x1a.us>
 // All rights reserved. This file is part of rpf, distributed under the
 // BSD 3-Clause license. For full terms please see the LICENSE file.
+#![doc(html_root_url = "https://0X1A.github.io/rpf")]
 
 //! rpf is a framework to help create Rust programs
 
